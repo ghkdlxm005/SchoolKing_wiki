25b32c21ef877f661d6fc179115bcdcb3233d185
mycatsvelly <ghkdlxm005@naver.com>
2026-06-23 23:40:25 +0900

    wiki update 2026-06-23 23:40:25.36

---
+++ src/theme/DocItem/Paginator/index.js
@@ -0,0 +1,5 @@
+// Override: hide the prev/next pagination at the bottom of doc pages.
+// We navigate via the sidebar tree + child-card lists instead.
+export default function Paginator() {
+  return null;
+}
